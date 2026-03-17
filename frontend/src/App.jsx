f2407481a75836292233b1966856acb92aacdbf7
Krishnapriya VM <krishnapriya4599@gmail.com>
2026-03-17 22:41:43 +0530

    feat: setup frontend UI with Tailwind CSS, daisyUI, react-hot-toast, lucide-react and integrate API using axios with CORS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetailPage from './pages/NoteDetailPage'
+import toast from 'react-hot-toast'
 
-export const App = () => {
+const App = () => {
   return (
-    <div>App</div>
+    <div data-theme = "forest">
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/create' element={<CreatePage/>} />
+        <Route path='/note/:id' element={<NoteDetailPage/>} />
+      </Routes>
+    </div>
   )
 }
+
+export default App
